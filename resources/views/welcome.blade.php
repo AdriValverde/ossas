ff45102dd467ca7d339f4e85ef7ff797de4bcb7a
damianfernandez <damiancerero@us.es>
2017-02-22 17:19:35 +0100

    Prueba commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CGIS: Citas Paciente
+                     1
                 </div>
 
                 <div class="links">
