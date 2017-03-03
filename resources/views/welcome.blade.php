2da0c996130c432b46ab4b8910ec7eda2c75158c
damianfernandez <damiancerero@us.es>
2017-03-03 13:19:50 +0100

    Mensaje obligatorio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                     CGIS: Citas Pacientas
+                     CGIS: Citas Pacientes CON CAMBIO
                 </div>
 
                 <div class="links">
