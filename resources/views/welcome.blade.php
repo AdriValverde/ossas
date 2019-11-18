4883f064a2cfacda3843f8bdc4c4e0dda1a90372
adrib <lEOPOLDOALAS103>
2019-11-18 17:34:30 +0100

    Nuevo título

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                     CGIS: Citas Pacientes OSSAS Cambio
+                     CGIS: Citas Pacientes OSSAS
                 </div>
 
                 <div class="links">
