53833d50f400e5d38dd1c786103927a938a43c9c
artmorart <armoar@gmail.com>
2019-12-02 02:23:42 +0100

    Migracion Location y modelo Location

---
+++ app/Location.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    protected $fillable = ['hospital', 'consulta'];
+
+    public function citas()
+    {
+        return $this->hasMany('App\Cita');
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->hospital .' Consulta:'.$this->consulta;
+    }
+}
