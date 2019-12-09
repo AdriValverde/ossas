8270c0456362dba6198106b6003b0b81505fa96d
artmorart <armoar@gmail.com>
2019-12-09 14:17:45 +0100

    Corrección en la migración create_pacientes_table y nueva migración add_enfermedad_to_paciente

--- database/migrations/2019_12_09_130807_add_enfermedad_to_paciente.php
+++ database/migrations/2019_12_09_130807_add_enfermedad_to_paciente.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePacientesTable extends Migration
+class AddEnfermedadToPaciente extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreatePacientesTable extends Migration
      */
     public function up()
     {
-        Schema::create('pacientes', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('nuhsa');
-            $table->timestamps();
-
+        Schema::table('pacientes', function (Blueprint $table) {
+            $table->unsignedInteger('enfermedad_id');
+            $table->foreign('enfermedad_id')->references('id')->on('enfermedads')->onDelete('cascade');
         });
     }
 
@@ -30,6 +26,6 @@ class CreatePacientesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('pacientes');
+        Schema::dropForeign('pacientes_enfermedad_id_foreign');
     }
 }
