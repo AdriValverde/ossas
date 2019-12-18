2e42ac9591420ee6c657440e329e77f11a54b2d3
aguarared <aarare@hotmail.com>
2019-12-18 11:48:01 +0100

    Seeders pacientes, doses y medicamento(4/10)v2

--- database/seeds/medicamentosSeeder.php
+++ database/seeds/medicamentosSeeder.php
@@ -32,10 +32,24 @@ class medicamentosSeeder extends Seeder
         ]);
         DB::table('doses')->insert([
             'nombre' => 'SUPOSITORIOS DE GLICERINA DR. TORRENTS ADULTOS ',
-            'composión' => 'Briviact 10 mg/ml',
-            'presemtación' => 'solución vía oral',
-            'link_vademecum' => 'https://www.vademecum.es/medicamento-briviact_prospecto_1151073021',
+            'composión' => 'GLICEROL 3,27 g',
+            'presemtación' => 'supositorio',
+            'link_vademecum' => 'https://www.vademecum.es/medicamento-supositorios+de+glicerina+dr.+torrents+adultos_prospecto_45647',
         ]);
+        DB::table('doses')->insert([
+            'nombre' => 'REFLEX Gel',
+            'composión' => '100 mg de salicilato de metilo, 60 mg de esencia de trementina, 30 mg de alcanfor y 30 mg de mentol.',
+            'presemtación' => 'gel de uso tópico',
+            'link_vademecum' => 'https://www.vademecum.es/medicamento-reflex_prospecto_59479',
+        ]);
+        DB::table('doses')->insert([
+            'nombre' => 'REFLEX Gel',
+            'composión' => '100 mg de salicilato de metilo, 60 mg de esencia de trementina, 30 mg de alcanfor y 30 mg de mentol.',
+            'presemtación' => 'gel de uso tópico',
+            'link_vademecum' => 'https://www.vademecum.es/medicamento-reflex_prospecto_59479',
+        ]);
+
+
 
 
 
